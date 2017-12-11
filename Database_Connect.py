78a252f5e63d81226da934b0d533c4c48d770b1e
zsw <zhangshiweiwork@gmail.com>
2017-12-11 16:19:24 +0800

    add function disposing datatime format in excel

--- Database_Connect.py
+++ Database_Connect.py
@@ -2,84 +2,67 @@ import pymysql
 import logging
 logging.basicConfig(filename='info.log', filemode="w", level=logging.DEBUG)
 
-class MyDataBase:
-    global db,cursor
-    #creat a database
-    #the db must conect the database
-    #databaseName is the name of your database
-    def creat_database(self,databaseName):
-        try:
-            self.cursor.execute("CREATE DATABASE {0} DEFAULT CHARACTER SET utf8mb4".format(databaseName))
-        except Exception as e:
-            print('creating tables error，error information：{0}'.format(e))
+#creat a database
+#the db must conect the database
+#databaseName is the name of your database
+def creat_database(db,databaseName):
+    cursor = db.cursor
+    try:
+        cursor.execute("CREATE DATABASE {0} DEFAULT CHARACTER SET utf8mb4".format(databaseName))
+    except Exception as e:
+        print('creating tables error，error information：{0}'.format(e))
 
-    #creat table
-    #the db must conect the database ,and must bind the database where you will create a table
-    #tableName is the name of your table
-    def creat_table(self,tableName):
-        sql = 'CREATE TABLE IF NOT EXISTS {0} (' \
-              'id INT(10) NOT NULL AUTO_INCREMENT,' \
-              'name VARCHAR(255) NOT NULL,title VARCHAR(255),' \
-              'content TEXT,time VARCHAR(255),' \
-              'source VARCHAR(255),url VARCHAR(255), ' \
-              'PRIMARY KEY(id))'.format(tableName)
-        self.cursor.execute(sql)
+#creat table
+#the db must conect the database ,and must bind the database where you will create a table
+#tableName is the name of your table
+def creat_table(db,tableName):
+    cursor = db.cursor()
+    sql = 'CREATE TABLE IF NOT EXISTS {0} (id INT(10) NOT NULL AUTO_INCREMENT,name VARCHAR(255) NOT NULL,title VARCHAR(255),content TEXT,time TIMESTAMP,source VARCHAR(255),url VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,deleted_at TIMESTAMP,PRIMARY KEY(id))'.format(tableName)
+    cursor.execute(sql)
 
 
-    #insert data to database
-    #the db must conect the database ,and must bind the database where you will create a table
-    #the variable name & data is the data
-    def insert(self,table,name,item):
-        sql = 'INSERT INTO {0}(name, title, content, time, source, url) ' \
-              'values(%s, %s, %s, %s, %s, %s)'.format(table)
-        try:
-            self.cursor.execute(sql, (name, item[0], item[1], item[2], item[3], item[4]))
-            self.db.commit()
-        except Exception as e:
-            db.rollback()
-            logging.info('insert error information：{0}'.format(e))
-            print('insert error information：{0}'.format(e))
+#insert data to database
+#the db must conect the database ,and must bind the database where you will create a table
+#the variable name & data is the data
+def insert(db,table,name,item):
+    cursor = db.cursor()
+    sql = 'INSERT INTO {0}(name, title, content, time, source, url) values(\'{1}\', \'{2}\',\'{3}\', \'{4}\', \'{5}\', \'{6}\')'.format(table,name, item[0], item[1], item[2], item[3], item[4])
+   # print(sql)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logging.info('error information：{0}'.format(e))
+        print('error information：{0}'.format(e))
 
-    #query function
-    def query(self,sql = ''):
-       try:
-           self.cursor.execute(sql)
-       except Exception as e:
-           db.rollback()
-           logging.info('select error information：{0}'.format(e))
-           print('query error information：{0}'.format(e))
+# get the version of mysql,this can be used to check
+# whether connecting the database seccessfully
+#the db must conect the database
+#the excute and fetch must belong to the same cursor
+def version_test(db):
+    cursor = db.cursor()
+    cursor.execute('SELECT VERSION()')
+    data = cursor.fetchone()
+    print('Database version:', data)
+    return data
 
-    #modify function
-    def  modify(self,sql = ' '):
-        try :
-            self.cursor.execute(sql) #执行sql
-            self.db.commit() #提交修改
-        except Exception as e:
-            db.rollback()
-            logging.info('select error information：{0}'.format(e))
-            print('modify error information：{0}'.format(e))
+def connect(myhost='localhost',myuser='root',mypassword='123456',myport=3306,mydb='secrank',mycharset="utf8"):
+    db = pymysql.connect(host=myhost,user=myuser, password=mypassword, port=myport,db=mydb,charset=mycharset)
+    return db
 
-    def fetchone(self):
-        return self.cursor.fetchone()
+def main():
+    db = connect('localhost','root', '123456', 3306,'secrank',"utf8mb4")
+    data = version_test(db)
+    logging.info(data)
+    creat_table(db,'company_news')
+   #  cursor = db.cursor()
+   #  sql = 'INSERT INTO company_news(name, time,deleted_at) ' \
+   #        'values(\'NetEye\', \'2017-01-13 21:35:23\',null)'
+   #  print(sql)
+   #  cursor.execute(sql)
+    db.commit()
+    db.close()
 
-    def fetchall(self):
-        return self.cursor.fetchall()
-
-
-
-    # get the version of mysql,this can be used to check
-    # whether connecting the database seccessfully
-    #the db must conect the database
-    #the excute and fetch must belong to the same cursor
-    def version_test(self):
-        self.cursor.execute('SELECT VERSION()')
-        data = self.cursor.fetchone()
-        print('Database version:', data)
-        return data
-
-    def connect(self,myhost,myuser,mypassword,myport,mydb,mycharset):
-        self.db = pymysql.connect(host=myhost,user=myuser, password=mypassword, port=myport,db=mydb,charset=mycharset)
-        self.cursor = self.db.cursor()
-
-    def close(self):
-        self.db.close()
+if __name__ == '__main__':
+    main()
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
+from datetime import date,datetime,time
 import xlrd
 from Database_Connect import insert
 import pymysql
 import logging
+import pandas
 
 logging.basicConfig(filename='info.log', filemode="w", level=logging.DEBUG)
 
@@ -23,17 +25,58 @@ def read_file(filename):
 
     #print('行：{0}    列：{1}'.format(nrows,ncols))
     data = getRow(nrows, ncols,table)
+
+
     return data
 
+def getStandardTime(table,i):
+   # print(table.name,i)
+   try:
+        tmp = xlrd.xldate.xldate_as_datetime(table.cell(i, 2).value, 1)
+        month = tmp.month
+        day = tmp.day
+        year = tmp.year - 4
+        if day == 1 and month == 1:
+            year -= 1
+            month = 12
+            day = 1
+        elif month == 3 and day == 1:
+            month = 2
+            if (year % 4):
+                day = 28
+            else:
+                day = 29
+        elif day == 1:
+            month -= 1
+            if (month in [1, 3, 5, 7, 8, 10, 12]):
+                day = 31
+            else:
+                day = 30
+        else:
+            day -= 1
+        # print(year,month,day)
+        return datetime(year, month, day, hour=tmp.hour, minute=tmp.minute, second=tmp.second)
+   except Exception as e:
+        print(e)
+        logging.info('   {0}'.format(e))
+        return datetime(2015, 2, 6, hour=0, minute=0, second=0)
 
 def getRow(nrows,ncols,table):
     data = []
     for i in range(1,nrows):
         rowdata=[]
+        #data.append(table.row_values(i)[2])
+
+       # tmptime = pandas.Timestamp("{0}-{1}-{2} {3}".format(tmp.year-4,tmp.month,tmp.day-1))
+       # print(tmp, '>', year,'-', month, '-', day, ' ', tmp.time())
+        #print('sss->>>>>>>>{0}'.format(tmp))
         for col in range(0,5):
             col_data = table.row_values(i)[col]
+            print(col_data,type(col_data),col_data=='')
             if(~isinstance(col_data,str)):
                 col_data = str(col_data)
+            if(col==2):
+                col_data=getStandardTime(table,i)
             rowdata.append(col_data)
         data.append(rowdata)  #某一行数据
     return data
@@ -41,22 +84,26 @@ def getRow(nrows,ncols,table):
 
 
 def database_modify(name,data):
-    db = pymysql.connect(host='localhost',user='root', password='123456', port=3306,db='tmp',charset="utf8mb4")
+    db = pymysql.connect(host='localhost',user='root', password='123456', port=3306,db='secrank',charset="utf8mb4")
     cursor = db.cursor()
+    count=1;
     for item in data:
-        print('   {0}'.format(item))
-        logging.info('   {0}'.format(item))
-        insert(db,'tmp',name,item)
+        print('{0}   {1}'.format(count,item))
+        #logging.info('   {0}'.format(item))
+        insert(db,'company_news',name,item)
+        count+=1
     db.close()
 
 
 def main():
-    rootdir = 'D:\Anaconda3\myproject\secrank_data_process\secrank_data'
+    rootdir = 'D:\Anaconda3\myproject\excel_transfer_mysql\secrank_data'
     list = os.listdir(rootdir)  # the list of the folder's child folder and file
     for i in range(0, len(list)):
         path = os.path.join(rootdir, list[i])
+        print(list[i])
         if os.path.isfile(path):
             data = read_file(path)
+            print('>>>>>>>>>>>>>>>',list[i].split('.')[0])
             #logging.info('transfering the {0} file>>>>>>>>{1}>>>>>>>'.format(i,list[i]))
             #print('transfering the {0} file>>>>>>>>{1}>>>>>>>'.format(i,list[i]))
             database_modify( list[i].split('.')[0] , data )
@@ -64,6 +111,8 @@ def main():
 #single file test
 def tmp():
     data = read_file('secrank_data/NetEye.xlsx')
+    print(data)
+
     database_modify('NetEye.xlsx'.split('.')[0], data)
 
 
